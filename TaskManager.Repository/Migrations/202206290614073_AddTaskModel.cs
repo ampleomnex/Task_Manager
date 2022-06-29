15cb764a76ac2fb1a5aec0e8ae77ec304480b386
sreelakshmy <urlakshmy@gmail.com>
2022-06-29 16:01:37 +0530

    Added  task model

---
+++ TaskManager.Repository/Migrations/202206290614073_AddTaskModel.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskManager.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTaskModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Task_tbl",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TaskName = c.String(nullable: false),
+                        PriorityID = c.Int(nullable: false),
+                        EstTime = c.Time(nullable: false, precision: 7),
+                        EmployeeID = c.Int(nullable: false),
+                        CreatedBy = c.Int(nullable: false),
+                        DueDate = c.DateTime(nullable: false),
+                        CreatedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Task_tbl");
+        }
+    }
+}
